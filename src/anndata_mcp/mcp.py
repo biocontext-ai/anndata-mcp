4e0455a69ac144406b69dcd0aea550e7f0a7867e
dschaub95 <schaub.darius@gmail.com>
2026-02-16 22:42:27 +0100

    update deprecated fastmcp argument

--- src/anndata_mcp/mcp.py
+++ src/anndata_mcp/mcp.py
@@ -3,5 +3,5 @@ from fastmcp import FastMCP
 mcp: FastMCP = FastMCP(
     name="anndata-mcp",
     instructions="Allows to retrieve information about an AnnData object via MCP",
-    on_duplicate_tools="error",
+    on_duplicate="error",
 )
